e69bdb83d212c03d075fbf23d24ca52476a0d81d
Ubuntu <ubuntu@ip-172-31-39-173.ec2.internal>
2020-05-12 15:09:21 +0000

    Added deployment script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t aapurvk/multi-client:latest -t aapurvk/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t aapurvk/multi-server:latest -t aapurvk/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t aapurvk/multi-worker:latest -t aapurvk/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push aapurvk/multi-client:latest
+docker push aapurvk/multi-server:latest
+docker push aapurvk/multi-worker:latest
+
+docker push aapurvk/multi-client:$SHA
+docker push aapurvk/multi-server:$SHA
+docker push aapurvk/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployments server=aapurvk/multi-server:$SHA
+kubectl set image deployments/client-deployments client=aapurvk/multi-client:$SHA
+kubectl set image deployments/worker-deployments worker=aapurvk/multi-worker:$SHA
